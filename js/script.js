d9918e76aea7d75350ef0641c70c6847434eda55
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2021-03-04 03:02:30 +0300

    first application (filmsRate)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,46 @@
-console.log(1);
+// "use strict";
+
+const numberOfFilms = +prompt('Сколько фильмом посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const films = prompt('Один из последних фильмов?', ''),
+    rate = +prompt('На сколько оцените его?', ''),
+    films1 = prompt('Один из последних фильмов?', ''),
+    rate1 = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[films] = rate;
+personalMovieDB.movies[films1] = rate1;
+
+console.log(personalMovieDB);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
