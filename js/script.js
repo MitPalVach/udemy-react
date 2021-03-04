150623f2244b3051bfad8b52fe7b7b65f28cd67d
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2021-03-04 22:21:37 +0300

    first application (filmsRate) numbers of movies, genres, privat, rate for 2 best movies

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмом посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмом посмотрели?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмом посмотрели?', '');
+    }
+}
+
+start();
+
+// ===
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -9,90 +21,82 @@ const personalMovieDB = {
     privat: false
 };
 
+// ===
 
-for (let i = 0; i < 2; i++) {
-    const films = prompt('Один из последних фильмов?', ''),
-        rate = +prompt('На сколько оцените его?', '');
-
-    if (films != null && rate != null && films != '' && rate != '' && films.length < 50) {
-        personalMovieDB.movies[films] = rate;
-        console.log('Yes');
-    } else {
-        console.log('error');
-        i--;
+function showMyDB() {
+    if (personalMovieDB["privat"] === false) {
+        console.log(personalMovieDB["privat"])
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log('malo');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-    console.log('norm');
-} else if (personalMovieDB.count > 30) {
-    console.log('mnogo');
-} else {
-    console.log('error');
+showMyDB();
+
+// ===
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        let quest = prompt(`Ваш любимый жанр под номером ${i}`);
+        if (quest != null && quest !== '' && quest.length < 15) {
+            personalMovieDB.genres[i - 1] = quest;
+        } else {
+            i--;
+        }
+    }
 }
 
-console.log(personalMovieDB);
+writeYourGenres();
 
+// ===
 
-// =====================================================================================================================
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const films = prompt('Один из последних фильмов?', ''),
+            rate = +prompt('На сколько оцените его?', '');
+        if (films != null && rate != null && films !== '' && rate !== '' && films.length < 30) {
+            personalMovieDB.movies[films] = rate;
+            console.log('Yes');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
 
-// const num = 50;
-// if (num < 44) {
-//     console.log('net');
-// } else if (num > 111) {
-//     console.log('net');
-// } else {
-//     console.log('da');
-// }
-// (num < 60) ? console.log('da') : console.log('net');
-//
-// const numb = 50;
-// switch (numb) {
-//     case 49:
-//         console.log('net');
-//         break;
-//     case 100:
-//         console.log('net');
-//         break;
-//     case 50:
-//         console.log('da');
-//         break;
-//     default:
-//         console.log('---');
-//         break;
-// }
+rememberMyFilms();
 
-// =====================================================================================================================
+// ======= или
 
-// let number1 = 50;
-// while (number1 < 55) {
-//     console.log(number1);
-//     number1++;
-// }
-
-// do {
-//     console.log(number1);
-//     number1++;
-// } while (number1 < 55);
-
-// for (let i = 1; i < 10; i++) {
-//     if (i) {
-//         // break;
-//         continue;
+// let p = 0;
+// while (p < 2) {
+//     const films = prompt('Один из последних фильмов?', ''),
+//         rate = +prompt('На сколько оцените его?', '');
+//     p++;
+//     if (films != null && rate != null && films != '' && rate != '' && films.length < 50) {
+//         personalMovieDB.movies[films] = rate;
+//         console.log('Yes');
+//     } else {
+//         console.log('error');
+//         p--;
 //     }
-//     console.log(i);
 // }
 
+// ===
 
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('malo');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        console.log('norm');
+    } else if (personalMovieDB.count > 30) {
+        console.log('mnogo');
+    } else {
+        console.log('error');
+    }
+}
 
+detectPersonalLevel();
 
-
-
-
-
-
+console.log(personalMovieDB);
 
 
 
---
+++ js/undo.js
@@ -0,0 +1,235 @@
+"use strict";
+//// ===================================================================================================================
+//
+// function createCounter() {
+//     let counter = 0;
+//     const myFunction = function () {
+//         counter = counter + 1;
+//         return counter
+//     }
+//     return myFunction
+// }
+// const increment = createCounter();
+// const c1 = increment();
+// const c2 = increment();
+// const c3 = increment();
+// console.log('example increment', c1, c2, c3);
+//
+//// ===
+// let val =7;
+// function createAdder() {
+//     function addNumbers(a,b) {
+//         let ret = a + b;
+//         return ret
+//     }
+//     return addNumbers;
+// }
+// let adder = createAdder();
+// let sum = adder(val, 8);
+// console.log('example of function returning a function: ', sum);
+//
+//// ===
+//
+// let vall = 2;
+// function multiplyThis(n) {
+//     let ret = n * vall;
+//     return ret
+// }
+// let multiplied = multiplyThis(6);
+// console.log('example of scope:', multiplied);
+//
+//// ===
+//
+// let a = 3;
+// function addTwo(x) {
+//     let ret = x + 2;
+//     return ret
+// }
+// let b = addTwo(a)
+// console.log(b)
+//
+//
+//
+//// ===================================================================================================================
+//
+// const num = 50;
+// if (num < 44) {
+//     console.log('net');
+// } else if (num > 111) {
+//     console.log('net');
+// } else {
+//     console.log('da');
+// }
+// (num < 60) ? console.log('da') : console.log('net');
+
+//// ===
+
+// const numb = 50;
+// switch (numb) {
+//     case 49:
+//         console.log('net');
+//         break;
+//     case 100:
+//         console.log('net');
+//         break;
+//     case 50:
+//         console.log('da');
+//         break;
+//     default:
+//         console.log('---');
+//         break;
+// }
+
+//// ===================================================================================================================
+
+// let number1 = 50;
+// while (number1 < 55) {
+//     console.log(number1);
+//     number1++;
+// }
+
+//// ====
+
+// for (let i = 1; i < 10; i++) {
+//     if (i) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+//// ====
+
+// do {
+//     console.log(number1);
+//     number1++;
+// } while (number1 < 55);
+//
+//// ===================================================================================================================
+//
+// let num = 20;
+//
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 12;
+//     console.log(num);
+// }
+//
+// showFirstMessage('Hello world!');
+// console.log(num);
+//
+//// ===
+//
+// function calc(a, b) {
+//     return (a + b);
+// }
+//
+// console.log(calc(4, 3));
+// console.log(calc(7, 5));
+// console.log(calc(11, 12));
+//
+//// ===
+//
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+//
+// const anotherNum = ret();
+// console.log(anotherNum);
+//
+//// ===
+//
+// const logger = function () {
+//     console.log('hi');
+// };
+//
+// logger();
+//
+//// ===
+//
+// const calc = (a, b) => {
+//    return  a + b
+// };
+//
+//
+//// ===================================================================================================================
+//
+// const fd = 'teSt-tEst-111';
+// const ddd = [1,2,3,4];
+//
+// console.dir(fd.toLowerCase());
+// console.log(ddd.length);
+//
+// let fruit = 'Some 23 fruit - is apple'
+// console.log(fruit.indexOf('uit'));
+//
+// const logg = '1234 5678 4321 8765 309';
+// const loog = (logg).slice(-8, -4);
+// console.log('XXXX ' + loog);
+// console.log(logg.substr(10, 3));
+//
+// const num = 12.2;
+// const numb = Math.round(num);
+// console.log(numb);
+//
+// const test = '12.3px';
+// console.log(parseFloat(test));
+//
+//// ===================================================================================================================
+
+
+
+
+
+//// ===================================================================================================================
+
+
+//// ===================================================================================================================
+
+
+//// ===================================================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
